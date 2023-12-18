b2b6ba0a76f48671216f93ca1e1d49fe14021263
marcosalv92 <marcosalv92@gmail.com>
2023-12-18 21:36:16 +0100

    Starting API REAprofile

---
+++ main.py
@@ -0,0 +1,9 @@
+from fastapi import FastAPI
+from routers.users import users
+
+app = FastAPI()
+app.include_router(users.router)
+
+@app.get('/')
+async def welcome_reaprofile():
+    return {'message': 'Welcome to REAprofile'}
